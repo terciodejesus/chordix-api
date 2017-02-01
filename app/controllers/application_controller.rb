071061c1de02a152be1f37366d2a9752943777d2
terciodejesus <terciodejesus@gmail.com>
2017-01-31 23:50:30 -0300

    Added CORS into application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   require 'open-uri'
 
+  after_filter :cors_set_access_control_headers
+
   def generate_html
     doc = Nokogiri::HTML(open("https://www.cifraclub.com.br/legiao-urbana/pais-filhos/simplificada.html"))
     render :text => doc
+  end
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
+    headers['Access-Control-Max-Age'] = "1728000"
   end
 end
