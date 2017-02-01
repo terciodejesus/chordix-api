e18365713795b8795fec9370e924bc142104b0aa
terciodejesus <terciodejesus@gmail.com>
2017-01-31 23:45:04 -0300

    Added CORS

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,9 @@ module ChordcipherApi
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Origin' => 'https://terciodejesus.github.io/chordcipher/',
+      'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+    }
   end
 end
